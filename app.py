3005685142f85c457492b2a5cca68595df5e9e8c
zaku_ <jakub.jacqes@gmail.com>
2024-10-07 20:00:20 +0200

    cleanup

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def get_files(req_path):
     return render_template('drive2.html' , data={
         'files': file_objs})   
 
-
-app.run(host= "0.0.0.0" , port=50100, debug=True)  
+if __name__ == '__main__':
+    app.run(host= "0.0.0.0" , port=50100, debug=True)  
